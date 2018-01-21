1bb9ea79ab244958c9c79ef4ca8f896a3046afdd
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2018-01-21 14:46:16 +0900

    クラスネーム付の方針を、black/white を用いる方向に変更

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -24,11 +24,40 @@ const keyArray = [
   20,
   21,
   22,
-  23]
+  23,
+]
+
+const keyColorPattern = [
+  true,
+  false,
+  true,
+  false,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  true,
+]
 
 const renderKeys = (keyArray) => {
   const keys = keyArray.map((o, i) => {
-    return <div className={`keyboard__key keyboard__key--${i}`}>{o}</div>
+    return <div key={i} className={`keyboard__key keyboard__key--${i} keyboard__key--${keyColorPattern[i]? 'white' : 'black'}`}>
+      <input value={o} className={`keyboard__input keyboard__input--${keyColorPattern[i]? 'white' : 'black'}`}/></div>
   })
 
   return <div className='keyboard'>{keys}</div>
